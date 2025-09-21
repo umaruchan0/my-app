97479d48778f2522ec7c9e3b8f1ddc0918a3d6dd
umaru <omarprogramer0@gmail.com>
2025-09-21 13:55:54 +0300

    work

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get("/api/prodact", (req, res) => {
 // });
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>404 - Page Not Found</h1>");
+  res.status(404).send("<h1>404 - Page Not Found</h1>");// fuck u
 });
 
 app.listen(port, () => {
